4c39d8ea439690dfa4e7f112b24e557e22a4f812
Milán Péter Novák <milan.novak@stud.uni-corvinus.hu>
2021-04-15 22:25:06 +0200

    8. gyak

--- HajosTeszt/Startup.cs
+++ HajosTeszt/Startup.cs
@@ -16,10 +16,11 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        /*public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -36,6 +37,39 @@ namespace HajosTeszt
                 {
                     //await context.Response.WriteAsync("Hello World!");
                 });
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }*/
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles(); //A sorrend fontos!
+
+            app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+
+
+            app.UseEndpoints(endpoints =>
+            {
+               endpoints.MapControllers();
             });
         }
     }
